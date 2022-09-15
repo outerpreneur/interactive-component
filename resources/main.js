6eea4096b6744174ad431709602b252de8fc3636
Aleksander <alek@outerpreneur.com>
2022-09-15 19:09:57 +0200

    showing rating  content

--- resources/main.js
+++ resources/main.js
@@ -1,5 +1,29 @@
 const mainContainer = document.querySelector(".container-box");
-const ThanksContainer = document.querySelector(".Thank-you");
+const thanksContainer = document.querySelector(".thank-you");
 
 const submitButton = document.getElementById("submit");
 const rateAgain = document.getElementById("rate-again");
+const rating = document.getElementById("rating");
+// get the numbered buttons
+const rates = document.querySelectorAll(".btn");
+
+submitButton.addEventListener("click", () => {
+  // when click remove class hidden
+  thanksContainer.classList.remove("hidden");
+  // when click remove container box
+  mainContainer.style.display = "none";
+});
+
+rateAgain.addEventListener("click", () => {
+  // when click add class hidden
+  thanksContainer.classList.add("hidden");
+  // when click show container box
+  mainContainer.style.display = "block";
+});
+
+rates.forEach((rate) => {
+  // when clicking rating button
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML;
+  });
+});
