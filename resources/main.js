c911ea2df63c8293b7e286d61532bbc0da8178e1
Aleksander <alek@outerpreneur.com>
2022-09-15 15:49:16 +0200

    javascript

---
+++ resources/main.js
@@ -0,0 +1,5 @@
+const mainContainer = document.querySelector(".container-box");
+const ThanksContainer = document.querySelector(".Thank-you");
+
+const submitButton = document.getElementById("submit");
+const rateAgain = document.getElementById("rate-again");
